660feb83dcddb04966f3c282c452da91373aa645
Julian Cabrera <a.j.c.marce@gmail.com>
2022-06-21 11:33:31 -0300

    adding likes and comments buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,24 +2,23 @@ import React from 'react'
 import { gql, useQuery } from '@apollo/client';
 import { Grid } from 'semantic-ui-react';
 
+import PostCard from '../components/PostCard';
+
 
 function Home() {
-  const { loading, error, data: {getPosts: posts } } = useQuery(FETCH_POST_QUERY);
-
-  if (posts) {
-  console.log(posts);
-  }
+  const { loading, error, data } = useQuery(FETCH_POST_QUERY);
   
-
   return (
     <Grid columns={3}>
-      <Grid.Row>
+      <Grid.Row className='page-title'>
         <h1>Recent Posts</h1>
       </Grid.Row>
     <Grid.Row>
-      <Grid.Column>
-        <Image src='/images/wireframe/media-paragraph.png' />
-      </Grid.Column>
+      { loading ? (<h1>Loading ...</h1>) : (data.getPosts && data.getPosts.map(post => (
+        <Grid.Column key={post.id} style={{marginBottom: 20}}>
+          <PostCard post={post} />
+        </Grid.Column>
+      )))}
     </Grid.Row>
     </Grid>
   )
